90716e47e7fb7bafaeb7d63c71877a2a7de6b6f9
jpwalkiewicz <jp.walkiewicz@gmail.com>
2017-03-26 19:25:21 +0200

    Dodanie dzialajacego slidera

--- js/js.js
+++ js/js.js
@@ -1,5 +1,57 @@
+var sliderChair = document.querySelectorAll(".sliderChair");
+var arrow_left = document.querySelector(".arrow_left");
+var arrow_right = document.querySelector(".arrow_right");
 
+arrow_left.addEventListener("click", function(event) {
+    console.log("Huuura clicknięto!");
+    if (sliderChair[0].dataset.image == 0) {
+        sliderChair[0].dataset.image = 1;
+        sliderChair[1].dataset.image = 0;
 
-var button = document.querySelector(".button");
-var menu = document.querySelector(".menu");
-counter = 0;
+        sliderChair[0].classList.remove("invisible");
+        sliderChair[1].classList.add("invisible");
+        sliderChair[2].classList.add("invisible");
+    } else if (sliderChair[2].dataset.image == 0) {
+        sliderChair[2].dataset.image = 1;
+        sliderChair[0].dataset.image = 0;
+
+        sliderChair[2].classList.remove("invisible");
+        sliderChair[0].classList.add("invisible");
+        sliderChair[1].classList.add("invisible");
+    } else if (sliderChair[1].dataset.image == 0) {
+        sliderChair[1].dataset.image = 1;
+        sliderChair[2].dataset.image = 0;
+
+        sliderChair[1].classList.remove("invisible");
+        sliderChair[2].classList.add("invisible");
+        sliderChair[0].classList.add("invisible");
+    }
+
+})
+
+arrow_right.addEventListener("click", function(event) {
+    console.log("clicnięto prawe !");
+    if (sliderChair[0].dataset.image == 0) {
+        sliderChair[0].dataset.image = 1;
+        sliderChair[1].dataset.image = 0;
+
+        sliderChair[0].classList.remove("invisible");
+        sliderChair[1].classList.add("invisible");
+        sliderChair[2].classList.add("invisible");
+    } else if (sliderChair[1].dataset.image == 0) {
+        sliderChair[1].dataset.image = 1;
+        sliderChair[2].dataset.image = 0;
+
+        sliderChair[1].classList.remove("invisible");
+        sliderChair[0].classList.add("invisible");
+        sliderChair[2].classList.add("invisible");
+    } else if (sliderChair[2].dataset.image == 0) {
+        sliderChair[2].dataset.image = 1;
+        sliderChair[0].dataset.image = 0;
+       
+        sliderChair[2].classList.remove("invisible");
+        sliderChair[1].classList.add("invisible");
+        sliderChair[0].classList.add("invisible");
+    }
+
+})
